44094d75b7918dba218c8516d8b00e1960e9f5c6
Matt Ellen <matt@matthewellen.co.uk>
2013-10-17 09:31:29 +0100

    Update spws.py
    updated for python 3

--- spws.py
+++ spws.py
@@ -3,11 +3,11 @@ import asyncore
 
 def runserver():
     s = Server('0.0.0.0', 8080, 'index.html')
-    print 'Server created on port 8080.'
+    print('Server created on port 8080.')
     asyncore.loop()
 
 if __name__ == '__main__':
     try:
         runserver()
     except KeyboardInterrupt:
-        print '\nGoodbye.'
+        print('\nGoodbye.')
